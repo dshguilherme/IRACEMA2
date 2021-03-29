d6282b398239d18ea77ed2b27a02596b164e8f1d
Guilherme H. da Silva <dshguilherme@gmail.com>
2021-03-29 09:09:25 -0300

    Added eval_point() for all ranks
    Ported CCurvePoint2, SurfacePointRAT3 and VolumePoint from IRACEMA 1.0; Naming is not ideal, but accessing these functions from the class through eval_point should be the proper way.

--- inst/Geometry/Geometry.m
+++ inst/Geometry/Geometry.m
@@ -5,6 +5,7 @@ classdef Geometry
         knots;
         points;
         p;
+        n;
     end
     
     methods
@@ -19,37 +20,103 @@ classdef Geometry
             obj.knots = knots;
             obj.p = p;
             
-            mult = 1;
+            n = zeros(rank,1);
+            
             for i=1:rank
-                mult = mult*(length(knots{i})-p(i)-1);
+                n(i) = length(knots{i})-p(i)-1;
             end
+            
+            obj.n = n;            
+            mult = prod(n);
             assert(numel(points(:)) == mult, "Error: invalid number of points. A B-Spline has (n-p) points per parametric direction")
             
             obj.points = points;
             
         end
         
-        function eval_point()
+        function [x,y,z] = eval_point(parametric_coordinate_array)
+            
+            assert(obj.rank == numel(parametric_coordinate_array),"Error: invalid number of parameter")
+                        
+            switch obj.rank
+                
+                case 1
+                    Pw = [];
+                    u = parametric_coordinate_array(1);
+                    nu = obj.n(1);
+                    pu = obj.p(1);
+                    U = obj.knots{1};
+                    
+                    point = CCurvePoint2(nu,pu,U,Pw,u);
+                    x = point.x;
+                    y = point.y;
+                    z = point.z;
+               
+                case 2
+                    Pw = [];
+                    
+                    u = parametric_coordinate_array(1);
+                    nu = obj.n(1);
+                    pu = obj.p(1);
+                    U = obj.knots{1}; 
+                    
+                    v = parametric_coordinate_array(2);
+                    nv = obj.n(2);
+                    pv = obj.p(2);
+                    V = obj.knots{2};
+                    
+                    point = SurfacePointRAT3(nu,pu,U,nv,pv,V,Pw,u,v);
+                    x = point.x;
+                    y = point.y;
+                    z = point.z;
+                
+                case 3
+                    Pw = [];
+                    u = parametric_coordinate_array(1);
+                    nu = obj.n(1);
+                    pu = obj.p(1);
+                    U = obj.knots{1}; 
+                    
+                    v = parametric_coordinate_array(2);
+                    nv = obj.n(2);
+                    pv = obj.p(2);
+                    V = obj.knots{2};
+                    
+                    
+                    w = parametric_coordinate_array(3);
+                    nw = obj.n(3);
+                    pw = obj.p(3);
+                    W = obj.knots{3};
+                    
+                    point = VolumePoint(nu,pu,U,nv,pv,V,nw,pw,W,u,v,w);
+                    x = point.x;
+                    y = point.y;
+                    z = point.z;                  
+           
+            end
         end
         
-        function reverse_eval()
+        function reverse_eval(varargin)
+            error('In development');
         end
         
-        function extract_boundaries()
+        function extract_boundaries(varargin)
+            error('Port from IRACEMA 1.0 underway');
         end
         
-        function knot_refine()
+        function knot_refine(varargin)
         end
         
-        function degree_elevate()
+        function degree_elevate(varargin)
         end
         
-        function k_refine()
+        function k_refine(varargin)
             degree_elevate()
             knot_refine()
         end
         
-        function build_derivative()
+        function build_derivative(varargin)
+            error('In development.');
         end
     end
     
