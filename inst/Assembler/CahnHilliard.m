98d08116b80c88359c7d916b790621a0826fff1c
Guilherme H. da Silva <dshguilherme@gmail.com>
2021-03-31 15:41:37 -0300

    Declaring Cahn-Hilliard
    adding the framework for Cahn-Hilliard methods and properties. Will change over time.

--- inst/Assembler/CahnHilliard.m
+++ inst/Assembler/CahnHilliard.m
@@ -2,18 +2,54 @@ classdef CahnHilliard < Assembler
     
     properties
         
+        lambda;
+        theta;
+        rho_0;
+        diffusivity;
+        L_0;
+        alpha;
+        c;
+        
     end
     
     methods
         
-        function build_stiffness()
+        function mu(obj)
             
         end
         
-        function build_mass()
+        function nabla_mu(obj)
+        
+        end
+        
+        function mobility(obj)
+        
+        end
+        
+        function nabla_mobility(obj)
+        
+        end
+        
+        function rho = initial_condition(obj)
+        
+                        
+        end
+        
+        function K = build_stiffness(obj)
             
         end
         
+        function K = predictor(obj)
+        
+        end
+        
+        function K = corrector(obj)
+        
+        end
+        
+        function alpha_next = adapt_time(obj)
+        
+        end
     end
     
 end
