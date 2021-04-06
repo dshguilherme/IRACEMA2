867a61878b75afbdceb2d5d00b9127030a7cdcb8
Guilherme H. da Silva <dshguilherme@gmail.com>
2021-04-06 10:59:39 -0300

    Added Boundary Conditions
    Dirichlet linear solver for Assembler class
    Neumann and Robin Boundary Conditions for Poisson class problems.

--- inst/Assembler/Poisson.m
+++ inst/Assembler/Poisson.m
@@ -46,6 +46,86 @@ classdef Poisson < Assembler
             K = sparse(K);
         end
         
+        function F = neumann_bc(F,obj,h,boundaries)
+            domains = boundaries(:,1);
+            dofs = boundaries(:,2);
+            d = obj.dimensions;
+            for i=1:numel(domains)
+                asb = Assembler("gauss",d,domains{i});
+                [qp, qw] = asb.quad_rule;
+                [global_basis_index, element_local_mapping, element_ranges] = ...
+                    GetConnectivityArrays(domains{i});
+                [~, lm] = BuildGlobalLocalMatrices(element_local_mapping,d);
+                
+                n_quad = length(qw);
+                ndof = max(max(element_local_mapping))*d;
+                [nel_dof, nel) = size(element_local_mapping);
+                Fi = zeros(ndof,1);
+                
+                for e=1:nel
+                    F_e = zeros(nel_dof,1);
+                    for n=1:n_quad
+                        q = qp(n,:);
+                        [R, ~, J] = FastShape(domains{i}, q, global_basis_index, ...
+                            element_local_mapping, element_ranges, e);
+                        Jmod = abs(J*qw(n));
+                        F_e = F_e +Jmod*h*(R');
+                    end
+                    idx = lm(:,e)';
+                    Fi(idx) = Fi(idx) +F_e(:);
+                end
+                
+                basis = boundaries{i,2};
+                basis = basis(:);
+                F(basis) = F(basis)+Fi;
+            end
+        end
+            
+        function [K,F] = robin_bc(K,F,obj,r,beta,boundaries)
+            domains = boundaries(:,1);
+            dofs = boundaries(:,2);
+            d = obj.dimensions;
+            for i=1:numel(domains)
+                asb = Assembler("gauss",d,domains{i});
+                [qp, qw] = asb.quad_rule;
+                [global_basis_index, element_local_mapping, element_ranges] = ...
+                    GetConnectivityArrays(domains{i});
+                [~, lm] = BuildGlobalLocalMatrices(element_local_mapping,d);
+
+                n_quad = length(qw);
+                ndof = max(max(element_local_mapping))*d;
+                [nel_dof, nel) = size(element_local_mapping);
+                Fi = zeros(ndof,1);
+                Ki = zeros(ndof);
+
+                for e=1:nel
+                    K_e = zeros(nel_dof);
+                    F_e = zeros(nel_dof,1);
+                    for n=1:n_quad
+                        q = qp(n,:);
+                        [R, ~, J] = FastShape(domains{i}, q, global_basis_index, ...
+                            element_local_mapping, element_ranges, e);
+                        Jmod = abs(J*qw(n));
+                        F_e = F_e +Jmod*r*(R');
+                        N = kron(R', eye(d));
+                        K_e = K_e +Jmod*beta*(N'*N);
+                    end
+                    idx = lm(:,e)';
+                    Ki(idx,idx) = Ki(idx,idx) + K_e;
+                    Fi(idx) = Fi(idx) +F_e(:);
+                end
+
+                basis = boundaries{i,2};
+                basis = basis(:);
+                F(basis) = F(basis)+Fi;
+                K(basis,basis) = K(basis,basis) +Ki;
+            end
+
+                
+            end
+           
+        end
+        
     end
     
 end
